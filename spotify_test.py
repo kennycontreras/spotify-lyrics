6af3e40bb1f36c90cf68d346358933b8f2a7e7ab
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-03-27 12:25:37 -0300

    Added a functionality to shows the lyric of the current song

--- spotify_test.py
+++ spotify_test.py
@@ -1,7 +1,8 @@
 import sys
 import spotipy
 import spotipy.util as util
-import codecs
+import requests
+from bs4 import BeautifulSoup
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -19,7 +20,24 @@ if token:
     current_song = sp.currently_playing()
     artist = current_song['item']['artists'][0]['name']
     name_song = current_song['item']['name']
-    print(artist, name_song)
+    song_url = '{}-{}-lyrics'.format(str(artist).strip().replace(' ', '-'),
+                                     str(name_song).strip().replace(' ', '-'))
+    print(song_url)
+
+    print('\nSong: {}\nArtist: {}'.format(name_song, artist))
+
+    # Requests and BeautifulSoup code to show the lyrics of actual songs
+
+    request = requests.get("https://genius.com/{}".format(song_url))
+
+    # Veryfique if we found a lyric
+    if request.status_code == 200:
+        html_code = BeautifulSoup(request.text, features="html.parser")
+        lyric = html_code.find("div", {"class": "lyrics"}).get_text()
+        print(lyric)
+
+    else:
+        print("Sorry, I can't find the actual song")
 
 else:
     print("Can't get token for", username)
